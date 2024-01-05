d6c979e7b79b5d7399350a19a7a73d46825318d0
AydinCakmak <146542170+AydinCakmak@users.noreply.github.com>
2024-01-05 22:39:28 +0100

    Update Test2.java

--- src/Test2.java
+++ src/Test2.java
@@ -1,6 +1,7 @@
 public class Test2 {
     public static void main(String[] args) {
-        System.out.println("Hafta sonu odevi2");
+        System.out.println("Hafta sonu odevi2 YAPILDI: REMOT; COK EGLENCELI BIR ODEV");
         System.out.println("Yepyeni satirlar eklendi ve commit ypaildi ");
+        //REMOTE DA YAPILAN YEPYENI DEGISIKLIK
     }
 }

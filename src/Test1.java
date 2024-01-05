e0d7a3579159d66875dd0e23118eff5c7b09894b
Aydin Cakmak <aydin-cakmak45@hotmail.com>
2024-01-05 22:37:46 +0100

    Test1 and Test2 yeni degisiklikler yapildi

--- src/Test1.java
+++ src/Test1.java
@@ -1,5 +1,6 @@
 public class Test1 {
     public static void main(String[] args) {
         System.out.println("Hafta sonu odevi1");
+        System.out.println("Yeni satir eklendi ve commit yapildi");
     }
 }
--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,6 @@
 public class Test2 {
     public static void main(String[] args) {
         System.out.println("Hafta sonu odevi2");
+        System.out.println("Yepyeni satirlar eklendi ve commit ypaildi ");
     }
 }

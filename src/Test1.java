be02048ef2d6431bf81568b157daddfc8d0635a3
AydinCakmak <146542170+AydinCakmak@users.noreply.github.com>
2024-01-05 22:40:49 +0100

    Update Test1.java

--- src/Test1.java
+++ src/Test1.java
@@ -1,6 +1,7 @@
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Hafta sonu odevi1");
+        System.out.println("Hafta sonu odevi1 KODDA DEGISIKLIK YAPILDI");
         System.out.println("Yeni satir eklendi ve commit yapildi");
+        //REMOTE YORUM SATIRI EKLENDI
     }
 }
